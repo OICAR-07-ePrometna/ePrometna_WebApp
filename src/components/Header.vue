4b11e5104dcadcdb5ec4d228574d5f8027905c28
Tiitmouse <lkovacic380@gmail.com>
2025-03-31 16:25:16 +0200

    Added Header and Navigation components, refactored layout and template structure

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+      <v-container>
+        <v-app-bar :elevation="3" color="primary">
+          <v-app-bar-title class="header">ePrometna</v-app-bar-title>
+        </v-app-bar>
+      </v-container>
+    </v-app>
+  </template>
+
+<style lang="css" scoped>
+.header{
+    color: white;
+    font-weight: 900
+}
+</style>
+  
