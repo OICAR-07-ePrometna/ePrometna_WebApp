3bb3000434bf72eb248ff54ba6ed7e00ff8a2c39
Tiitmouse <lkovacic380@gmail.com>
2025-03-31 16:25:16 +0200

    Added Header and Navigation components, refactored layout and template structure

---
+++ src/components/Navigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-navigation-drawer>
+        <v-list-item class="title"> {{ items[0] }} </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item
+          v-for="(item, index) in items.slice(1)"
+          :key="index"
+          link
+          :class="{ logout: item === 'Odjava' }"
+        >
+          {{ item }}
+        </v-list-item>
+        <v-list-item class="title">Profil</v-list-item>
+        <v-divider></v-divider>
+        <v-list-item item >Detalji</v-list-item>
+        <v-list-item item class="logout">Odjava</v-list-item>
+      </v-navigation-drawer>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  items: {
+    type: Array as () => string[],
+    required: true,
+  },
+});
+</script>
+
+<style lang="css" scoped>
+.title {
+  font-weight: 900;
+}
+.logout {
+  color: lightcoral;
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <v-main>
-    <router-view />
+    <Header />
+    <Navigation :items="items" />
   </v-main>
-
   <AppFooter />
 </template>
 
 <script lang="ts" setup>
-//
+import { ref } from 'vue';
+import Header from '../components/Header.vue';
+import Navigation from '../components/Navigation.vue';
+
+const items = ref<string[]>([]);
+items.value = ['Menus', 'menu1', 'menu2', 'menu3'];
 </script>
