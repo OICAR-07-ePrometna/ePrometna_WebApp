9ac0d18b6d00cea0645ba28e82a52e8626ca27c6
Tiitmouse <lkovacic380@gmail.com>
2025-04-01 21:23:31 +0200

    O7E-70 added mobile support for navigation drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,29 @@
 <template>
-      <v-container>
-        <v-app-bar :elevation="3" color="primary">
-          <v-app-bar-title class="header">ePrometna</v-app-bar-title>
-        </v-app-bar>
-      </v-container>
-  </template>
+  <v-container>
+    <v-app-bar :elevation="3" color="primary">
+      <v-btn 
+        icon 
+        @click="$emit('toggle-drawer')" 
+        class="toggle-drawer-btn hide-on-desktop"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+      <v-app-bar-title class="header">ePrometna</v-app-bar-title>
+    </v-app-bar>
+  </v-container>
+</template>
 
 <style lang="css" scoped>
-.header{
+.header {
     color: white;
-    font-weight: 900
+    font-weight: 900;
+}
+.hide-on-desktop {
+    display: none;
+}
+@media (max-width: 1280px) {
+    .hide-on-desktop {
+        display: inline-flex;
+    }
 }
 </style>
-  
