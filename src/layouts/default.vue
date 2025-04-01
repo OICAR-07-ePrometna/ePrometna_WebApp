ed529b9db019378dd514fdfb303ec07baa6e6ae1
Tiitmouse <lkovacic380@gmail.com>
2025-04-01 21:05:12 +0200

    O7E-70 footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,22 +1,15 @@
 <template>
-  <Header />
-  <Navigation :items="items" />
-  <v-main>
-  </v-main>
-  <AppFooter />
+
+  <Navigation />
+
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import Header from '@/components/Header.vue';
+
 import Navigation from '@/components/Navigation.vue';
 
-const items = ref<string[]>([]);
-items.value = ['Menus', 'menu1', 'menu2', 'menu3'];
 </script>
 
-<style scoped>
-.v-navigation-drawer__content{
-  width: 200px;
-}
+<style lang="css" scoped>
+
 </style>
