ce0524982c0d12f222e808707b50ebfae6ac069c
Tiitmouse <lkovacic380@gmail.com>
2025-04-01 21:05:12 +0200

    O7E-70 footer

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,31 +1,27 @@
 <template>
-    <v-container >
-      <v-navigation-drawer >
-        <v-list-item class="title"> {{ items[0] }} </v-list-item>
-        <v-divider></v-divider>
-        <v-list-item
-          v-for="(item, index) in items.slice(1)"
-          :key="index"
-          link
-          :class="{ logout: item === 'Odjava' }"
-        >
-          {{ item }}
-        </v-list-item>
-        <v-list-item class="title">Profil</v-list-item>
-        <v-divider></v-divider>
-        <v-list-item item >Detalji</v-list-item>
-        <v-list-item item class="logout">Odjava</v-list-item>
-      </v-navigation-drawer>
-    </v-container>
+  <Header />
+  <v-container>
+    <v-navigation-drawer width="200" mobile-breakpoint="lg">
+      <v-list-item class="title"> {{ items[0] }} </v-list-item>
+      <v-divider></v-divider>
+      <v-list-item v-for="(item, index) in items.slice(1)" :key="index" link :class="{ logout: item === 'Odjava' }">
+        {{ item }}
+      </v-list-item>
+      <v-list-item class="title">Profil</v-list-item>
+      <v-divider></v-divider>
+      <v-list-item link item>Detalji</v-list-item>
+      <v-list-item link item class="logout">Odjava</v-list-item>
+    </v-navigation-drawer>
+  </v-container>
+  <Footer />
 </template>
 
 <script lang="ts" setup>
-defineProps({
-  items: {
-    type: Array as () => string[],
-    required: true,
-  },
-});
+import { ref } from 'vue';
+import Header from '@/components/Header.vue'
+import Footer from '@/components/AppFooter.vue'
+
+const items = ref<string[]>(['Menus', 'menu1', 'menu2', 'menu3']);
 </script>
 
 <style lang="css" scoped>
