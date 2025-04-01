b6e9431394ec309755e5e4aa02064ecbde33d7f9
Tiitmouse <lkovacic380@gmail.com>
2025-04-01 20:24:56 +0200

    O7E-70 footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-app>
       <v-container>
         <v-app-bar :elevation="3" color="primary">
           <v-app-bar-title class="header">ePrometna</v-app-bar-title>
         </v-app-bar>
       </v-container>
-    </v-app>
   </template>
 
 <style lang="css" scoped>
