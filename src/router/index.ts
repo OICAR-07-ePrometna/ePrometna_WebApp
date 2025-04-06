61b3a866e985c593376fa0d6950090328a0cdc91
Tiitmouse <lkovacic380@gmail.com>
2025-04-06 16:38:54 +0200

    Mismatch between Glob Pattern and Regex for Route Extraction fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const pages = import.meta.glob('../pages/*.vue')
 
 const routes = Object.keys(pages).map((path) => {
-  const name = path.match(/\.\/pages\/(.*)\.vue$/)?.[1]
+  const name = path.match(/\.\.\/pages\/(.*)\.vue$/)?.[1]
   const component = pages[path]
 
   return {
