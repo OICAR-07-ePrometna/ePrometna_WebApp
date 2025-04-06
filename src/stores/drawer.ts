ac70d72e8d5911eaf5ca0ba8cc7fb245e36c0a43
Tiitmouse <lkovacic380@gmail.com>
2025-04-06 14:10:52 +0200

    O7E-70 drawer rendering update

--- src/stores/drawer.ts
+++ src/stores/drawer.ts
@@ -2,13 +2,29 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useDrawer = defineStore('drawer', () => {
-  const open = ref(false);
+  const open = ref(window.matchMedia('(min-width: 1280px)').matches);
+
+  const updateOpenState = () => {
+    open.value = window.matchMedia('(min-width: 1280px)').matches;
+  }; 
+
   const toggle = () => {
     open.value = !open.value;
   };
   const setOpen = (value: boolean) => {
     open.value = value;
   };
+
+  onMounted(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1280px)');
+    mediaQuery.addEventListener('change', updateOpenState);
+    updateOpenState(); // Set initial state
+  });
+
+  onUnmounted(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1280px)');
+    mediaQuery.removeEventListener('change', updateOpenState);
+  });
   return {
     open,
     toggle,
